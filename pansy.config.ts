a2b4400b8755cec5b6a226f426eed647b67f004f
wangxingkang <156148958@qq.com>
2019-11-02 21:02:14 +0800

    feat: 替换打包工具 优化代码

---
+++ pansy.config.ts
@@ -0,0 +1,9 @@
+import { Config } from '@pansy/cli';
+
+const config: Config = {
+  input: {
+    index: 'src/index.ts'
+  }
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import { IApi, IConfig } from '@walrus/types';
-import replace from './replace';
+import replace, { ReplaceInFileConfig as ReplaceOptions } from 'replace-in-file';
 
-export default function(
-  api: IApi,
-  config: IConfig
-) {
+export default function(api: IApi, config: IConfig) {
   api.registerCommand('replace', {
     description: 'replace files keywords',
     usage: 'walrus replace [options] [...files]',
   }, (args, rawArgv, config) => {
-    replace(config).then(() => {});
+    try {
+      const results = replace.sync(config as ReplaceOptions);
+      console.log('Replacement results:', results);
+    }
+    catch (error) {
+      console.error('Error occurred:', error);
+    }
   })
+}
+
+export {
+  ReplaceOptions
 }
